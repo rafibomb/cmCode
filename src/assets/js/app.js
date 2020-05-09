c6488b54a081f694bdc2794da0f6c0ca7cc5dc8a
rafibomb <rafi@zurb.com>
2020-05-09 10:15:14 -0700

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,5 @@
+import 'what-input';
+
+require('./toggle-nav-tabbar');
+
+console.log('These are not the errors you are looking for ;)');
---
+++ src/assets/js/toggle-nav-tabbar.js
@@ -0,0 +1,47 @@
+// separate out functions
+// set active on first without click
+// play with cool timings
+// change left to translateX
+// add resize event to update widths
+// scope it -> iffee
+
+// add local times in separate file
+
+const navTabbar = document.getElementById('navTabbar');
+
+if (navTabbar) {
+  // ---------------
+  // variables
+  // ---------------
+
+  let links, underline;
+
+  links = document.querySelectorAll('.nav-tabbar-links .nav-tabbar-item');
+  underline = document.querySelector('.nav-tabbar-underline');
+
+  // ---------------
+  // events
+  // ---------------
+
+  // ---------------
+  // functions
+  // ---------------
+
+  function init() {
+    links[0].classList.add('is-active');
+    underline.style.left = links[0].getBoundingClientRect().left + 'px';
+    underline.style.width = links[0].clientWidth + 'px';
+  }
+
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function () {
+      for (var j = 0; j < links.length; j++) {
+        links[j].classList.remove('is-active');
+      }
+      this.classList.add('is-active');
+      underline.style.width = this.clientWidth + 'px';
+      underline.style.left = this.getBoundingClientRect().left + 'px';
+    });
+  }
+  init();
+}
